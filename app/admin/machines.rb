b566b0c5a65921cc1dd8fc3ccef78125ae8f5504
Raghu Betina <raghu@firstdraft.com>
2018-12-04 08:25:16 +0000

    Whitelist columns in Machine for ActiveAdmin

--- app/admin/machines.rb
+++ app/admin/machines.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Machine do
+
+ permit_params :name, :price, :service, :photo
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
