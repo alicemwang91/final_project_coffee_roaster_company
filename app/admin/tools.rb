e70ac4b5295372805f2cf60680e6099a45d43bd2
Raghu Betina <raghu@firstdraft.com>
2018-12-04 08:25:20 +0000

    Whitelist columns in Tool for ActiveAdmin

--- app/admin/tools.rb
+++ app/admin/tools.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Tool do
+
+ permit_params :name, :price, :photo
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
